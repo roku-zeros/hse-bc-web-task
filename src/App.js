297c8f7dd15a3a986ba6ae8c0b38814bd49dd903
roku-zeros <rob.zgonnikov@gmail.com>
2023-11-13 23:33:37 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,14 @@ import './App.css';
 import {CardBody} from "react-bootstrap";
 
 const App = () => {
+
+        axios.interceptors.request.use((config) => {
+            config.headers['Access-Control-Allow-Origin'] = '*'; // Установите заголовок Access-Control-Allow-Origin
+            return config;
+        });
+
+
+
     const handleButtonClick = () => {
         const postData = {
             login: 'frolova.a.e@sovkom.bank',
@@ -32,10 +40,3 @@ const App = () => {
 };
 
 export default App;
-app.options('/url...', function(req, res, next){
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Methods', 'POST');
-    res.header("Access-Control-Allow-Headers", "accept, content-type");
-    res.header("Access-Control-Max-Age", "1728000");
-    return res.sendStatus(200);
-});
